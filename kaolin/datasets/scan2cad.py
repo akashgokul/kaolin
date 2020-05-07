f58a1c1361eef41cc669a7dc5fe827fd7c24bd0a
akashgokul <akashgokul@berkeley.edu>
2020-05-06 17:33:03 -0700

    trying to train on more data

--- kaolin/datasets/scan2cad.py
+++ kaolin/datasets/scan2cad.py
@@ -133,7 +133,7 @@ class Scan2CAD(object):
             self.validation_data_frame.reset_index(inplace=True)
             self.validation_filepaths =  self.validation_data_frame['Filepath']
             self.validation_cad_ids = self.validation_data_frame['ID']
-            print([id for id in self.train_cad_ids if id not in self.validation_cad_ids.unique()])
+            print(len([id for id in self.train_cad_ids.unique() if id not in self.validation_cad_ids.unique()]))
             print("----"*10)
 
 
@@ -141,7 +141,7 @@ class Scan2CAD(object):
             self.test_data_frame.reset_index(inplace=True)
             self.test_filepaths =  self.test_data_frame['Filepath']
             self.test_cad_ids = self.test_data_frame['ID']
-            print([id for id in self.train_cad_ids if id not in self.test_cad_ids.unique()])
+            print(len([id for id in self.train_cad_ids.unique() if id not in self.test_cad_ids.unique()]))
 
             train_indices_lst = self.train_data_frame.index.tolist()
             # val_indices_lst = self.validation_data_frame.index.tolist()
